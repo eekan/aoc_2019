0b6f4d98a5943e57035911b15f8a12fe0b7103e9
Erik Ekstrom <erik@ecan.se>
2019-12-07 17:10:56 +0100

    Add solution to day 06, part 2

--- day_06/src/main.kt
+++ day_06/src/main.kt
@@ -53,7 +53,52 @@ fun part1() {
     println("Part 01: Total number of orbits: ${countOrbits(createTree(loadInput("day_06/input.txt")), 0)}")
 }
 
+fun findPath(nodeId : String, node: TreeNode<String>): List<String> {
+    if (node.value == nodeId)
+        return listOf(node.value)
+
+    for (child in node.children) {
+        val subPath = findPath(nodeId, child)
+        if (subPath.isNotEmpty())
+            return listOf(node.value) + subPath
+    }
+
+    return listOf()
+}
+
+fun findMinNumTransfers(source: String, destination: String, tree: TreeNode<String>): Int {
+    val pathToSource = findPath(source, tree).toMutableList()
+    val pathToDestination = findPath(destination, tree).toMutableList()
+
+    if (pathToDestination.isEmpty()) throw IllegalStateException("Destination ($destination) not found in tree")
+    if (pathToSource.isEmpty()) throw IllegalStateException("Source ($source) not found in tree")
+
+    val commonPath = pathToDestination.intersect(pathToSource)
+
+    // Remove common path
+    pathToSource.removeAll(commonPath)
+    pathToDestination.removeAll(commonPath)
+
+    // Number of transfer is equal to the combined path length, excluding the source and destination elements them self
+    return pathToDestination.size - 1 + pathToSource.size -1
+}
+
+fun example2() {
+    val orbits = listOf("COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J",
+            "J)K", "K)L", "K)YOU", "I)SAN")
+    val tree = createTree(orbits)
+
+    assertEquals(4, findMinNumTransfers("YOU", "SAN", tree))
+}
+
+fun part2() {
+    println("Required transfers from YOU to SAN: ${findMinNumTransfers("YOU", "SAN",
+            createTree(loadInput("day_06/input.txt")))}")
+}
+
 fun main() {
     example1()
     part1()
+    example2()
+    part2()
 }
